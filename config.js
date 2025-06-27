a66f92b1f8394894bf8a5d356cd0d496128be26c
Paul Maddaloni <pmaddaloni@gmail.com>
2025-06-26 17:00:30 -0700

    [updated] some config and packages.

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const config = {
     PORT: process.env.PORT,
     STEAM_API_KEY: process.env.STEAM_API_KEY,
     STEAM_GAME_UPDATES_SECRET: process.env.STEAM_GAME_UPDATES_SECRET,
+    ENVIRONMENT: process.env.NODE_ENV || 'development',
 };
 
 export default config;
